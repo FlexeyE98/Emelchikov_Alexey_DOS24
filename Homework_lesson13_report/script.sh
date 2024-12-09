9e0bd1750b60d3efc77142ebc4bc4fe599999e46
Alexey Emelchikov <128730329+FlexeyE98@users.noreply.github.com>
2024-12-09 19:58:54 +0300

    Update script.sh

--- Homework_lesson13_report/script.sh
+++ Homework_lesson13_report/script.sh
@@ -3,7 +3,10 @@
 person=$1
 
 function getPerson() {
-curl -s -X GET "https://swapi.dev/api/people/$person/" -H "Content-Type:application/json" | jq '.' 
+curl -s -X GET "https://swapi.dev/api/people/?search=$person/" -H "Content-Type:application/json" | jq '.' 
 }
 
 getPerson $person
+
+
+///Ищем персонажа + фильмы + планеты где обитает и тд
